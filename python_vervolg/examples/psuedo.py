5183208527f262f951f9cbc441cc1822d392c5d0
William Chau <w.chau1@student.han.nl>
2025-11-10 10:07:38 +0100

    add pseudo.py

---
+++ python_vervolg/examples/psuedo.py
@@ -0,0 +1,21 @@
+tekst = """
+- genereer een willekeurig getal genereer_getal() 
+- vraag gebruiker om getal vraag_getal()
+- laat zien of de gebruiker het getal heeft geraden antwoord()
+- vraag of de gebruiker nog een keer wilt spelen nog_een_keer_spelen()   
+"""
+
+def genereer_getal():
+
+def vraag_getal():
+    getal = input("noem een getal: ")
+    return getal
+
+def main():
+
+    getal = vraag_getal()
+    print(getal)
+
+
+main()
+
