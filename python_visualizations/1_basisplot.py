d435dc14b81778f16e860dfbd70aee4c2e42b4e2
William Chau <w.chau1@student.han.nl>
2025-11-11 10:40:15 +0100

    modify structure

--- python_visualizations/1_basisplot.py
+++ python_visualizations/1_basisplot.py
@@ -5,7 +5,7 @@ x = [1, 2, 3]
 y = [2, 4, 6]
 
 plt.plot(x, y)          # lijnplot
-plt.title("Eerste plot")
-plt.xlabel("x")
-plt.ylabel("y")
+# plt.title("Eerste plot")
+# plt.xlabel("x")
+# plt.ylabel("y")
 plt.show()
