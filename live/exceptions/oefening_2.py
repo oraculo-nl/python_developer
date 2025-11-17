8ea1513aa9b3b91e03398a4ca8525e87b7c246ce
oraculo-nl <info@oraculo.nl>
2025-11-17 14:50:48 +0100

    add kolommen  check

--- live/exceptions/oefening_2.py
+++ live/exceptions/oefening_2.py
@@ -1,15 +1,13 @@
 import csv
 
-# csv.DictReader()
-
 def lees_config(pad):
     try:
         with open (pad, newline='') as csvfile:
             r = csv.DictReader(csvfile)
             for row in r:
-                if len(row) != 3:
-                    raise ValueError()
-                print(row)
+                if len(row) != 2:
+                    raise ValueError("deze regel heeft niet het vereiste aantal kolommen: "+str(r.line_num))
+                print(row['a'])
     except FileNotFoundError:
         print("file niet gevonden")
     except OSError:
